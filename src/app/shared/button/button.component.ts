de9dfcca256fb22c708074839f00817c41d647ae
saliou33 <losaliou.sl33@gmail.com>
2023-01-08 15:16:30 +0000

    Update HomePage

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,10 +1,11 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
+
 export class ButtonComponent {
   @Input() text: string = '';
   @Input() color: string = 'text-neutral10';
@@ -12,5 +13,6 @@ export class ButtonComponent {
   @Input() link: string = '#';
   @Input() px: string= 'px-4';
   @Input() py: string = 'py-3';
-
+  @Input() shadow: string = 'sm';
+  @Input() hover: string = '';
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,15 @@ import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { ButtonComponent } from './button/button.component';
+import { BrandComponent } from './brand/brand.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    BrandComponent
   ],
   imports: [
     CommonModule,
@@ -19,7 +21,8 @@ import { ButtonComponent } from './button/button.component';
   ],
   exports: [
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    BrandComponent
   ]
 })
 export class SharedModule { }
